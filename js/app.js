7ff26a95883a5a56d7d33c43d21184b038e0d6f4
alex-whan <whan.alexander@gmail.com>
2020-05-11 13:14:17 -0700

    Added constructor function and associated prototypes

---
+++ js/app.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var parent = document.getElementById('product');
+var allProducts = [];
+
+function ProductImage(){
+  this.filePath = url;
+  this.alt = alt;
+  this.title = title;
+  this.views = 0;
+  this.votes = 0;
+  allProducts.push(this);
+}
+
+new ProductImage('img/bag.jpg', 'bag', 'bag')
+new ProductImage('img/banana.jpg', 'banana', 'banana')
+new ProductImage('img/bathroom.jpg', 'bathroom', 'bathroom')
+new ProductImage('img/boots.jpg', 'boots', 'boots')
+new ProductImage('img/breakfast.jpg', 'breakfast', 'breakfast')
+new ProductImage('img/bubblegum.jpg', 'bubblegum', 'bubblegum')
+new ProductImage('img/chair.jpg', 'chair', 'chair')
+new ProductImage('img/cthulhu.jpg', 'cthulhu', 'cthulhu')
+new ProductImage('img/dog-duck.jpg', 'dog-duck', 'dog-duck')
+new ProductImage('img/dragon.jpg', 'dragon', 'dragon')
+new ProductImage('img/pen.jpg', 'pen', 'pen')
+new ProductImage('img/pet-sweep.jpg', 'pet-sweep', 'pet-sweep')
+new ProductImage('img/scissors.jpg', 'scissors', 'scissors')
+new ProductImage('img/shark.jpg', 'shark', 'shark')
+new ProductImage('img/sweep.jpg', 'sweep', 'sweep')
+new ProductImage('img/tauntaun.jpg', 'tauntaun', 'tauntaun')
+new ProductImage('img/unicorn.jpg', 'unicorn', 'unicorn')
+new ProductImage('img/usb.gif', 'usb', 'usb')
+new ProductImage('img/water-can.jpg', 'water-can', 'water-can')
+new ProductImage('img/wine-glass.jpg', 'wine-glass', 'wine-glass')
+
+ProductImage.prototype.renderImage = function(){
+  // create an element - img
+  var imageElement = document.createElement('img');
+  // fill the src with the path to the specific product
+  imageElement.setAttribute('src', this.filePath);
+  // fill in the alt which is going to be the name of the image minus the file extension (.jpg)
+  imageElement.setAttribute('alt', this.alt);
+  // fill in the title with the same text as the alt
+  imageElement.setAttribute('title', this.title);
+  // appendChild to parent element (imageElement)
+  parent.appendChild(imageElement);
+}
+
+// Helper function to get the random number
+function randomNumber(min = 0, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Render THREE random images to DOM from the product image array
+
+function getRandomProduct(){
+// First give an empty string to parent to clear all content
+  parent.textContent = '';
+
+  // Call random number function to get random product index position from allProducts array
+
+  var firstRandomIndex = randomNumber(0, allProducts.length-1);
+  var secondRandomIndex = randomNumber(0, allProducts.length-1);
+  var thirdRandomIndex = randomNumber(0, allProducts.length-1);
+
+  while(firstRandomIndex === secondRandomIndex || firstRandomIndex === thirdRandomIndex || secondRandomIndex === thirdRandomIndex){
+    secondRandomIndex = randomNumber(0, allProducts.length-1);
+    thirdRandomIndex = randomNumber(0, allProducts.length-1);
+  }
+  
+  allProducts[firstRandomIndex].renderImage();
+  allProducts[firstRandomIndex].views++;
+
+  allProducts[secondRandomIndex].renderImage();
+  allProducts[secondRandomIndex].views++;
+
+  allProducts[thirdRandomIndex].renderImage();
+  allProducts[thirdRandomIndex].views++;
+
+}
+
+// Create function to handle the click
+
+// set up event listener
+
+parent.addEventListener('click', function(){
+  var titleOfProductThatWasClickedOn = event.target.title;
+
+  for(var i=0; i<allProducts.length; i++){
+    if(titleOfProductThatWasClickedOn === allProducts[i].title){
+      allProducts[i].votes++
+    }
+  }
+
+getRandomProduct();
+
+});
+
