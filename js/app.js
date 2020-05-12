ea31d8cde1488c637573bbf349cfa11c35b7a95f
alex-whan <whan.alexander@gmail.com>
2020-05-12 16:50:23 -0700

    Streamlined functions, added makeNamesArray/generateChart functions

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function ProductImage(name, extension){
   allProducts.push(this);
 }
 
-ProductImage.prototype.renderImage = function(){
+ProductImage.prototype.render = function(){
   // create an element - img
   var imageElement = document.createElement('img');
   // fill the src with the path to the specific product
@@ -26,7 +26,7 @@ ProductImage.prototype.renderImage = function(){
   // fill in the title with the same text as the alt
   imageElement.setAttribute('title', this.title);
   // appendChild to parent element (imageElement)
-  parent.appendChild(imageElement);
+  parentElement.appendChild(imageElement);
 }
 
 
@@ -52,28 +52,26 @@ new ProductImage('usb', '.gif');
 new ProductImage('water-can', '.jpg');
 new ProductImage('wine-glass', '.jpg');
 
-
-
-
 function getRandomIndex(){
-  var index = getRandomNumber(allProducts.length)
+  var index = getRandomNumber(allProducts.length);
+// Get a random image from the product array to render
+// Use helper function to generate a random number between 0 and the array length
+// render that object instance at that index to the DOM
+
+  // while loop will check to ensure index is unique from the last 3 products shown (not in uniqueIndexArray)
   while(uniqueIndexArray.includes(index)){
     index = getRandomNumber(allProducts.length);
   }
 
   uniqueIndexArray.push(index);
 
+  // if array is more than 3 products long, shift from beginning in order to add new products
   if(uniqueIndexArray.length > 3){
     uniqueIndexArray.shift();
   }
 
   return index;
 }
-
-
-
-
-
 
 // Helper function to get the random number
 function getRandomNumber(max){
@@ -82,68 +80,99 @@ function getRandomNumber(max){
 
 // Render THREE random images to DOM from the product image array
 
-function getRandomProduct(){
-// First give an empty string to parent to clear all content
-  parent.textContent = '';
-
-  // Call random number function to get random product index position from allProducts array
-
-  var firstRandomIndex = randomNumber(0, allProducts.length-1);
-  var secondRandomIndex = randomNumber(0, allProducts.length-1);
-  var thirdRandomIndex = randomNumber(0, allProducts.length-1);
-
-  while(firstRandomIndex === secondRandomIndex || firstRandomIndex === thirdRandomIndex || secondRandomIndex === thirdRandomIndex){
-    secondRandomIndex = randomNumber(0, allProducts.length-1);
-    thirdRandomIndex = randomNumber(0, allProducts.length-1);
-  }
-  
-  allProducts[firstRandomIndex].renderImage();
-  allProducts[firstRandomIndex].views++;
-
-  allProducts[secondRandomIndex].renderImage();
-  allProducts[secondRandomIndex].views++;
-
-  allProducts[thirdRandomIndex].renderImage();
-  allProducts[thirdRandomIndex].views++;
-
+function displayImage(){
+  var index = getRandomIndex();
+  allProducts[index].render();
+  allProducts[index].views++;
 }
-
-// Create function to handle the click
-// set up event listener
-parent.addEventListener('click', clickHandler);
 
 // Event handler
 function clickHandler(event){
+  // Empty everything out first
+  parentElement.textContent = '';
+
+  // Identify what was clicked (target)
   var titleOfProductThatWasClickedOn = event.target.title;
 
+  // Loop through all object instances to compare titles - this will identify the target/clicked object
   for(var i=0; i<allProducts.length; i++){
     if(titleOfProductThatWasClickedOn === allProducts[i].title){
+      // Add a vote to that product
+      // Subtract one round from survey (counter)
       allProducts[i].votes++
       numberOfRounds--;
-    } 
+   
+      if (numberOfRounds===0) {
+        // Turn off event listener
+        parentElement.removeEventListener('click', clickHandler);
+        makeNamesArray();
+      }   
+    }
   }
 
-  if (numberOfRounds===0) {
-    parent.removeEventListener('click', clickHandler);
-    displayResults();
-  } else {
-    getRandomProduct();
-  }
-};
-
-// function to display results of survey once the number of rounds maxes out (default is 25 rounds)
-function displayResults(){
-  parent.textContent = '';
-  parent = document.getElementById('results');
-  var listElement = document.createElement('ul');
-
-  for(var i=0; i<allProducts.length; i++){
-    listElement = document.createElement('li');
-    listElement.textContent = `${allProducts[i].title} had ${allProducts[i].votes} votes and was shown ${allProducts[i].views} times.`
-    parent.appendChild(listElement);
-  }
-  parent.appendChild(listElement);
+  displayImage();
+  displayImage();
+  displayImage();
+  // Listen for click - render new images when we hear it
+  // Track views and votes for each image
 }
 
-getRandomProduct();
+displayImage();
+displayImage();
+displayImage();
+
+parentElement.addEventListener('click', clickHandler);
+
+// Loop over all products and make an array of just the names
+function makeNamesArray(){
+  for(var i=0; i<allProducts.length; i++){
+    names.push(allProducts[i].title);
+    votes.push(allProducts[i].votes);
+  }
+
+  generateChart();
+}
+
+function generateChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: names,
+          datasets: [{
+              label: '# of Votes',
+              data: votes,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+}
+
+
+
 
